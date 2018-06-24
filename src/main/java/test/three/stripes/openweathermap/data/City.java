2434d534cd868470010112dcbc5cda096f3c9eb1
alexandre <alexandretnparaujo@gmail.com>
2018-06-24 11:21:16 -0300

    Adding more cities

--- src/main/java/test/three/stripes/openweathermap/data/City.java
+++ src/main/java/test/three/stripes/openweathermap/data/City.java
@@ -4,8 +4,12 @@ public enum City {
 
     RIO_DE_JANEIRO(3451190, "Rio de Janeiro", -22.91, -43.21),
     WROCLAW(3081368, "Wroclaw", 51.1, 17.03),
-    ZARAGOZA(3104324, "Zaragoza", -0.88, 41.65),
-    MONTAIN_VIEW(420006353, "Mountain View", -122.08, 37.39);
+    ZARAGOZA(6362983, "Zaragoza",41.65, -0.88),
+    MONTAIN_VIEW(420006353, "Mountain View", 37.39, -122.08),
+    BIRKIRKARA(2563191, "Birkirkara", 35.54, 14.27),
+    MODICA(2524119, "Modica", 36.8499, 14.76976),
+    BACHURINO(578680, "Bachurino", 55.58, 37.52),
+    KOMUNARKA(545782, "Kommunarka", 55.5695, 37.4893);
 
     private Integer id;
     private String name;
